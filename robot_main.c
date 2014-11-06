d76511a78447b2b00533e390b3f6167b157156fa
Chris Johns <cajohns.93@gmail.com>
2014-11-06 15:46:33 +0000

    Clean up the project.
    Added gitignore
    Formatted code
    Commented code
    Fixed missing include

--- robot_main.c
+++ robot_main.c
@@ -1,88 +1,63 @@
 #include "e_epuck_ports.h"
+#include "e_init_port.h"
 #include "e_motors.h"
 #include "e_prox.h"
 
-#define LIGHT_WAIT 1
+#define PROX_THRESHOLD 100
 
-
-int GetSelector() {
-	return SELECTOR0 + 2*SELECTOR1 + 4*SELECTOR2 + 8*SELECTOR3;
-}
-
-void SwitchLED(int led, int on)
-{
-    switch (led)
-    {
-        case 0:
-            LED0 = on;
-            break;
-        case 1:
-            LED1 = on;
-            break;
-        case 2:
-            LED2 = on;
-            break;
-        case 3:
-            LED3 = on;
-            break;
-        case 4:
-            LED4 = on;
-            break;
-        case 5:
-            LED5 = on;
-            break;
-        case 6:
-            LED6 = on;
-            break;
-        case 7:
-            LED7 = on;
-            break;
-
-        default:
-            LED0 = on;
-            LED1 = on;
-            LED2 = on;
-            LED3 = on;
-            LED4 = on;
-            LED5 = on;
-            LED6 = on;
-            LED7 = on;
+/*
+ * Wait for 100K noops to prevent
+ * the poor epuck from exploding...
+ */
+void wait() {
+    long i;
+    for (i = 0; i < 100000; i++) {
+        asm("nop");
     }
 }
 
-int main(void)
-{
-	int prox7, prox0;
-	long i;
+int main(void) {
+    int prox7, prox0;
 
     e_init_port();
-	e_init_motors();
-	e_init_prox();
+    e_init_motors();
+    e_init_prox();
 
-	//e_set_speed_left(500); //go forward on half speed
- 	//e_set_speed_right(-500); //go backward on half speed
-    
-	while(1)
-    {
+    while (1) {
+        /*
+         * Fetch the values from the IR blasters
+         */
         prox7 = e_get_prox(7);
-		prox0 = e_get_prox(0);
+        prox0 = e_get_prox(0);
 
-		if(prox0 > 100 || prox7 > 100) {
-			prox0 *= 10;
-			prox7 *= 10;
+        /*
+         * If the IR values are below the
+         * threshold, assume it's just noise
+         * and sit still
+         */
+        if (prox0 > PROX_THRESHOLD || prox7 > PROX_THRESHOLD) {
+            prox0 *= 10;
+            prox7 *= 10;
 
-			if (prox0 > 1000)
-				prox0 = 1000;
+            /*
+             * Cap the value to the maximum
+             * speed of the epucks motors.
+             *
+             * The motors have a range of
+             * -1000 : 1000
+             */
+            if (prox0 > 1000)
+                prox0 = 1000;
 
-			if (prox7 > 1000)
-				prox7 = 1000;
+            if (prox7 > 1000)
+                prox7 = 1000;
 
-			e_set_speed_left(prox0);
-			e_set_speed_right(prox7);
-		} else {
-			e_set_speed_left(0);
-			e_set_speed_right(0);
-		}
-		for(i=0; i<100000; i++) { asm("nop"); }
+            e_set_speed_left(prox0);
+            e_set_speed_right(prox7);
+        } else {
+            e_set_speed_left(0);
+            e_set_speed_right(0);
+        }
+        wait();
     }
 }
